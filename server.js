bf75c5d4c5133ef22baefa9e7610565412efb8d9
Dmitry Ryazantsev <dika1993@yandex.ru>
2018-11-17 14:09:22 +0300

    Реализована серверная часть на server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const port = 4000;
+const statLocation = __dirname + "/posts.txt";
+
+app.use(express.static(__dirname));
+
+let totalPosts = parseInt(fs.readFileSync(statLocation));
+
+app.get("/save", (req, res) => {
+    totalPosts += parseInt(req.query.errors);
+
+    // запись в файл
+    fs.writeFile(statLocation, totalPosts, (err) => {
+        if (err) {
+            console.log("Ошибка записи в файл!");
+        }
+    });
+
+    // отправляем статус ответа 200 (без текста)
+    res.sendStatus(200);
+});
+
+app.get("/posts", (req, res) => {
+
+    res.send(totalPosts);
+});
+
+
+app.listen(port, () => {
+    console.log(`Сервер запущен на порту ${port}`) ;
+});
